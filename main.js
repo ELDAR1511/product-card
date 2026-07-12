de4899a2bea3bd9ef1849017528fd059b6d75749
Eldar Butasov <eldar15111984@gmail.com>
2026-07-12 23:31:00 +0300

    correction of comments

--- main.js
+++ main.js
@@ -5,33 +5,43 @@ const changeColorAllCardsButton = document.querySelector('#change-color-all-card
 const greenColorHash = '#00FF00';
 const blueColorHash = '#0000FF';
 
-changeColorAllCardsButton.addEventListener('click', () => 
-{productCards.forEach(card => {card.style.backgroundColor = greenColorHash;});});
+changeColorAllCardsButton.addEventListener('click', () => {
+  productCards.forEach(card => {
+    card.style.backgroundColor = greenColorHash;
+  });
+});
 
 // Изменение цвета первой карточки
 
 const firstProductCard = document.querySelector('.card');
 const changeColorFirstCardButton = document.querySelector('#change-color-first-card');
 
-changeColorFirstCardButton.addEventListener('click', () => 
-{firstProductCard.style.backgroundColor = blueColorHash;});
+changeColorFirstCardButton.addEventListener('click', () => {
+  firstProductCard.style.backgroundColor = blueColorHash;
+});
 
 // Открытие страницы Google.com
 
 const openGoogleButton = document.querySelector('#open-google');
 
-openGoogleButton.addEventListener('click', () => {openGoogle();});
+openGoogleButton.addEventListener('click', () => {
+  openGoogle(); 
+});
 
 function openGoogle() {
   const answer = confirm('Вы действительно хотите открыть страницу Google.com?');
-  if (answer===true) {window.open('https://www.google.com');}else {return;}
+  if (answer === true) {
+    window.open('https://www.google.com');
+  } else {
+    return;
+  }
 }
 
 // Вывод консоль лог
 
 const outputLogButton = document.querySelector('#output-console-log');
 
-outputLogButton.addEventListener('click', () => 
+outputLogButton.addEventListener('click', () =>
   outputConsoleLog('Привет, мир!'));
 
 function outputConsoleLog(message) {
@@ -41,14 +51,16 @@ function outputConsoleLog(message) {
 
 //Вывод заголовка в консоль
 
-const h1=document.getElementsByTagName('h1');
+const productTitle = document.getElementsByTagName('h1');
 
-h1[0].addEventListener('mouseover', () => {console.log('Выбери свой продукт!')});
+productTitle[0].addEventListener('mouseover', () => {
+   console.log('Выбери свой продукт!') 
+  });
 
 //Кнопка меняющая свой цвет при нажатии
 
 const changeColorButton = document.querySelector('#change-color-button');
 
 changeColorButton.addEventListener('click', () => {
-changeColorButton.classList.toggle('active');
+  changeColorButton.classList.toggle('active');
 })
