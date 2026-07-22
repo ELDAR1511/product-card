1caecf738b93e46f286c4c15a4e33e6093d27871
Eldar Butasov <eldar15111984@gmail.com>
2026-07-22 13:46:36 +0300

    created new function

--- homework-7.js
+++ homework-7.js
@@ -6,7 +6,50 @@ function weather(city, temperature) {
 
 weather('Черкесск', 20);
 
-//Скорость света
+//Задание 4
 
-const speedOfLight = 299792458; // м/с
+let speedOfLight = 299792458; // м/с
 
+if (speedOfLight > 299792458) {
+  console.log('Сверхсветовая скорость');
+} else if (speedOfLight === 299792458) {
+  console.log('Скорость света');
+} else {console.log('Субсветовая скорость');
+
+}
+
+//Задание 5
+
+let product = 'Телефон';
+let price = 1000;
+
+function buyProduct(budget) {
+  if (budget >= price) {
+    console.log(`${product} приобретен. Спасибо за покупку!`);
+  } else {
+    let difference = price - budget;
+    console.log(`Вам не хватает ${difference}$, пополните баланс.`);
+  }
+}
+
+buyProduct(900);
+buyProduct(1000);
+
+//Задание 6
+
+function accessControl(age) {
+  if (age >= 18) {
+    console.log("Вход на территорию разрешён.");
+  } else {
+    console.log("Вход на территорию запрещён.");
+  }
+}
+
+accessControl(20);
+accessControl(16);
+
+//Задание 7
+
+let name = 'Эльдар';
+let age = 41;
+let city = 'Черкесск';
