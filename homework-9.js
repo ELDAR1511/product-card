9c21363a28e0e9fe45344d9b23a370346db54280
Eldar Butasov <eldar15111984@gmail.com>
2026-08-09 23:32:00 +0300

    created new js file

---
+++ homework-9.js
@@ -0,0 +1,16 @@
+// Задание- 2
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const result = numbers.filter(number => number >= 5);
+
+console.log(result);
+
+// Задание- 3
+
+const cars = ['mercedes', 'bmw', 'audi', 'toyota', 'mitsubishi'];
+
+const allCars = cars.includes('bmw');
+
+console.log(allCars);
+
