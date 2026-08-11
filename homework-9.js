adef4dcd3016274034041a2734445205fdae9afb
Eldar Butasov <eldar15111984@gmail.com>
2026-08-11 16:47:40 +0300

    exported new array

--- homework-9.js
+++ homework-9.js
@@ -25,4 +25,53 @@ console.log(reverseArray(cars));
 
 // Задание- 7
 
-import { comments } from "./comments";
+import { comments } from "./comments.js";
+
+console.log(comments.filter(comment => comment.email.includes('.com')));
+
+// Задание- 8
+
+const newComments = comments.map(comment => {
+  return {
+    ...comment,
+    postId: comment.id <= 5 ? 2 : 1
+  };
+});
+
+console.log(newComments);
+
+// Задание- 9
+
+const shortComments = comments.map(comment => ({
+  id: comment.id,
+  name: comment.name
+}));
+
+console.log(shortComments);
+
+// Задание- 10
+
+const longComments = comments.map(comment => ({
+  ...comment,
+  isInvalid: comment.body.length > 180
+}));
+
+console.log(longComments);
+
+// Задание- 11
+
+const emails = comments.reduce((acc, comment) => {
+  acc.push(comment.email);
+  return acc;
+}, []);
+
+console.log(emails);
+
+const newEmails = comments.map(comment => comment.email);
+
+console.log(newEmails);
+
+// Задание- 12
+
+console.log(newEmails.toString());
+console.log(newEmails.join(', '));
