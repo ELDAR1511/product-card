4aa04279b378047661aed9d6a0eeca8836c661c1
Eldar Butasov <eldar15111984@gmail.com>
2026-09-04 19:23:29 +0300

    created objects

---
+++ products.js
@@ -0,0 +1,42 @@
+const cards = [
+  {
+    id: 1,
+    name: "Увлажняющая маска",
+    description: "Способствует удерживанию влаги в верхних слоях кожи.",
+    category: "для нормальной кожи",
+    price: 3500,
+    image: "images/photo_2.png"
+  },
+  {
+    id: 2,
+    name: "Гель для умывания",
+    description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
+    category: "для нормальной кожи",
+    price: 1650,
+    image: "images/photo_3.png"
+  },
+  {
+    id: 3,
+    name: "Увлажняющий мусс",
+    description: "Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.",
+    category: "для нормальной кожи",
+    price: 2750,
+    image: "images/photo_1.jpg"
+  },
+  {
+    id: 4,
+    name: "Подарочный набор №1",
+    description: "Набор, состоящий из увлажняющего крема и маски.",
+    category: "для нормальной кожи",
+    price: 4750,
+    image: "images/photo_4.png"
+  },
+  {
+    id: 5,
+    name: "Подарочный набор №5",
+    description: "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.",
+    category: "для нормальной кожи",
+    price: 7520,
+    image: "images/photo_5.png"
+  }
+];
