0f1bdff62dc9a25fdbb72e3ebe96f141575e9b71
Eldar Butasov <eldar15111984@gmail.com>
2026-09-05 20:00:22 +0300

    created new objects

--- products.js
+++ products.js
@@ -1,42 +1,58 @@
 const cards = [
   {
-    id: 1,
+    image: "images/photo_2.png",
+    category: "для нормальной кожи",
     name: "Увлажняющая маска",
     description: "Способствует удерживанию влаги в верхних слоях кожи.",
-    category: "для нормальной кожи",
+    compound: [ "воски", "минералы", "масла" ],
     price: 3500,
-    image: "images/photo_2.png"
+    
   },
   {
-    id: 2,
+    image: "images/photo_3.png",
+    category: "для нормальной кожи",
     name: "Гель для умывания",
     description: "Интенсивно очищает, не повреждает защитный барьер кожи.",
-    category: "для нормальной кожи",
+    compound: [ "минералы", "витамины С, А, РР, В И Е", "солнцезащитные компоненты" ],
     price: 1650,
-    image: "images/photo_3.png"
+    
   },
   {
-    id: 3,
+    image: "images/photo_1.jpg",
+    category: "для нормальной кожи",
     name: "Увлажняющий мусс",
     description: "Глубоко увлажняют кожу лица, оставляя её мягкой и гладкой.",
-    category: "для нормальной кожи",
-    price: 2750,
-    image: "images/photo_1.jpg"
-  },
+    compound: [ "активные натуральные комплексы", "витамины С, А, РР, В И Е", "солнцезащитные компоненты" ],
+    price: 2750
+  },  
   {
-    id: 4,
+    image: "images/photo_4.png",
+    category: "для нормальной кожи",
     name: "Подарочный набор №1",
     description: "Набор, состоящий из увлажняющего крема и маски.",
-    category: "для нормальной кожи",
-    price: 4750,
-    image: "images/photo_4.png"
+    compound: [ "воски", "минералы", "масла" ],
+    price: 4750
   },
   {
-    id: 5,
+    image: "images/photo_5.png",
+    category: "для нормальной кожи",
     name: "Подарочный набор №5",
     description: "Весь набор средств Invisible symphony, крем, маска, мусс и гель для умывания.",
-    category: "для нормальной кожи",
-    price: 7520,
-    image: "images/photo_5.png"
+    compound: [ "воски", "минералы", "масла" ],
+    price: 7520
   }
 ];
+
+const productCardTemplate = document.querySelector("#product-card-template");
+const productList = document.querySelector(".product-list");
+
+cards.forEach(card => {
+  const cardClone = productCardTemplate.content.cloneNode(true);
+  cardClone.querySelector(".card-image").src = card.image;
+  cardClone.querySelector(".card-category").textContent = card.category;
+  cardClone.querySelector(".card-name").textContent = card.name;
+  cardClone.querySelector(".card-description").textContent = card.description;
+  const compoundList = cardClone.querySelector(".card-compound");
+  cardClone.querySelector(".card-price").textContent = card.price.toLocaleString() + " ₽";
+  productList.appendChild(cardClone);
+});
